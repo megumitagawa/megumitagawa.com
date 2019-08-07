a5ec3a62f2f45ae755c7294af78fed5d9ff14f29
dai <chamestica@gmail.com>
2019-08-07 14:55:13 +0900

    Reset CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,9 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+
+import "sanitize.css"
+import "reset-css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -31,16 +34,7 @@ const Layout = ({ children }) => {
         title={data.site.siteMetadata.title}
         description={data.site.siteMetadata.description}
       />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <main>{children}</main>
       <Footer title={data.site.siteMetadata.title} />
     </>
   )

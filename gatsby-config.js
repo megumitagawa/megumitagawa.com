49f5afeadbde91e8412ae0a82c230d9fd7b06d33
dai <chamestica@gmail.com>
2019-08-18 00:34:21 +0900

    Use Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-svg-sprite`
+    `gatsby-plugin-svg-sprite`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GATSBY_GA_ID,
+        head: true,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

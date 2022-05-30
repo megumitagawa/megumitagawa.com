cd4e8c33295734fe41a9df9531b2716e92db2dd1
dsktschy <chamestica@gmail.com>
2022-05-31 08:59:54 +0900

    chore: create empty components

---
+++ src/components/PrimaryHeading.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import Vue from 'vue'
+
+type Data = {}
+type Methods = {}
+type Computed = {}
+type Props = {}
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+  name: 'PrimaryHeading',
+})
+</script>

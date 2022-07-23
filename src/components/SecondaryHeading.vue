d373d11e1aad25b85a6fa385ce3c93fb28a25e26
dsktschy <chamestica@gmail.com>
2022-07-23 16:17:34 +0900

    chore: style SecondaryHeading

--- src/components/SecondaryHeading.vue
+++ src/components/SecondaryHeading.vue
@@ -1,5 +1,8 @@
 <template>
-  <BaseBox :component="component" class="block w-full">
+  <BaseBox
+    :component="component"
+    class="block w-full text-base border-b border-inherit pb-4.5"
+  >
     <slot />
   </BaseBox>
 </template>

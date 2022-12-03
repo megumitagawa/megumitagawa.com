83dccf36b61ebb0156d9f6d543d09290eae8b774
dsktschy <chamestica@gmail.com>
2022-12-03 16:52:16 +0900

    build: set runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,28 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+
+  runtimeConfig: {
+    // Must not be empty
+    // https://github.com/contentful/contentful.js/blob/beta-v10/lib/contentful.ts#L37
+    contentfulCreateClientParams: {
+      accessToken: 'dummy',
+      space: 'dummy',
+    },
+    public: {
+      // Must not be empty
+      // https://github.com/contentful/contentful.js/blob/beta-v10/lib/contentful.ts#L37
+      contentfulCreateClientParams: {
+        accessToken: 'dummy',
+        space: 'dummy',
+      },
+      indexFullBodyImageDelay: 0,
+      indexPageBackdropDelay: 0,
+      indexPageWorkListLength: 5,
+      metaRobotsNone: false,
+      netlifyFormName: '',
+      siteUrl: '',
+      worksPageWorkListLength: 20,
+    },
+  },
 })
